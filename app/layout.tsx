6cbe3b5021725bf8899262e5bce053cd5813bd5d
David Baiye <ozerlihashem@gmail.com>
2025-04-15 21:19:12 +0100

    Name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "PromptlyBUilt",
+  title: "PromptlyBuilt",
   description: "Create a Beautiful Portfolio From Curated Templates",
 };
 
